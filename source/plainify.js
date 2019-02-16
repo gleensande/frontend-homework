72419dccb53baa3b3636c6e23fc79e41c0066409
Irina Nikiforova <gleensande@gmail.com>
2019-02-17 01:53:23 +0300

    var -> let

--- source/plainify.js
+++ source/plainify.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const plainify = function plainify(complicated) {
-  var simple = {};
+  let simple = {};
   processObj(complicated, '', simple);
 
   return simple;
 }
 
 function processObj(obj, prev, simp) {
-  for (var prop in obj) {
-    var val = obj[prop];
+  for (let prop in obj) {
+    let val = obj[prop];
 
     if (typeof(val) !== 'object') {
       processPair(prev + prop, val, simp);
