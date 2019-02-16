58f5f24d539828d1f821d539410f26be3d29c393
Irina Nikiforova <gleensande@gmail.com>
2019-02-17 01:57:15 +0300

    comments in JSDoc format

--- source/plainify.js
+++ source/plainify.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Создает plain-объект из объекта со вложенными свойствами
+ *
+ * @param {object} complicated - объект со вложенными свойствами
+ * @return {object} simple - plain-объект
+ */
 const plainify = function plainify(complicated) {
   if (typeof(complicated) !== 'object' || complicated === null) {
     return null;
@@ -11,6 +17,15 @@ const plainify = function plainify(complicated) {
   return simple;
 }
 
+/**
+ * Рекурсивная функция, обрабатывающая один объект.
+ * Записывает данные в переданную переменную.
+ *
+ * @param {object} obj - обрабатываемый в данный вызов объект
+ * @param {string} prev - строка, к которой будут прибавлены названия полей
+ * текущего объекта для перевода в plain-формат
+ * @param {object} simp - внешний объект, в который записывается plain
+ */
 function processObj(obj, prev, simp) {
   for (let prop in obj) {
     let val = obj[prop];
@@ -23,4 +38,11 @@ function processObj(obj, prev, simp) {
   }
 }
 
+/**
+ * Функция, записывающая ключ и значение в указанный объект
+ *
+ * @param {string} property - ключ
+ * @param {string} value - значение
+ * @param {object} simple - объект для записи
+ */
 let processPair = (property, value, simp) => simp[property] = value;
