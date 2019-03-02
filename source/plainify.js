83915887c847d7413fa56bde6e3e783cc51e3ca4
Irina Nikiforova <gleensande@gmail.com>
2019-03-02 20:16:20 +0300

    new version of returning simple object

--- source/plainify.js
+++ source/plainify.js
@@ -4,41 +4,41 @@
  * Создает plain-объект из объекта со вложенными свойствами
  *
  * @param {object} compl - объект со вложенными свойствами
- * @return {object} simple - plain-объект
+ * @return {object} simp - plain-объект
  */
 const plainify = (compl) => {
   if (!compl || typeof compl !== 'object') {
     return null;
   }
 
+  let simp = processObj(compl, '');
+
+  return simp;
+}
+  
+/**
+ * Рекурсивная функция, обрабатывающая один объект.
+ * Записывает данные в переданную переменную.
+ *
+ * @param {object} compl - обрабатываемый в данный вызов объект
+ * @param {string} prev - строка, к которой будут прибавлены названия полей
+ * текущего объекта для перевода в plain-формат
+ * @return {object} simp - plain-объект
+ */
+const processObj = (compl, prev) => {
   let simp = {};
-  let prev = '';
+  let complArr = Object.entries(compl);
 
-  /**
-   * Рекурсивная функция, обрабатывающая один объект.
-   * Записывает данные в переданную переменную.
-   *
-   * @param {object} compl - обрабатываемый в данный вызов объект
-   * @param {string} prev - строка, к которой будут прибавлены названия полей
-   * текущего объекта для перевода в plain-формат
-   * @return {object} simple - plain-объект
-   */
-  const processObj = (compl, prev) => {
-    let complArr = Object.entries(compl);
+  complArr.forEach( function(element) {
+    let [prop, val] = element;
 
-    complArr.forEach( function(element) {
-      let [prop, val] = element;
+    if (typeof val === 'object') {
+      simp = Object.assign(simp, processObj(val, prev + prop + '.'));
+      return simp;
+    }
 
-      if (typeof val === 'object') {
-        processObj(val, prev + prop + '.');
-        return;
-      }
-
-      simp[prev + prop] = val;
-    });
-  }
-
-  processObj(compl, prev);
+    simp[prev + prop] = val;
+  });
 
   return simp;
 }
